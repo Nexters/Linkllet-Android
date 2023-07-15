d0b5573117a166f7a37ca7dedb9b6e60755919f3
eshc123 <eshc@naver.com>
2023-07-15 21:31:59 +0900

    [chore] LnkTextField 패키지 수정

--- core/ui/src/main/java/com/linkedlist/linkllet/core/ui/LnkTextField.kt
+++ core/ui/src/main/java/com/linkedlist/linkllet/core/ui/LnkTextField.kt
@@ -1,11 +1,10 @@
-package ui
+package com.linkedlist.linkllet.core.ui
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
