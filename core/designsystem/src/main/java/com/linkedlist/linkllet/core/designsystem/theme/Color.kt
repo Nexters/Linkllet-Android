1b5ed20c9fdb3ae597f7fdcb5d1960540af05962
eshc123 <eshc@naver.com>
2023-07-15 23:10:27 +0900

    [add] Color 추가 - E0E0E0, BCBCBC, ColorF4F4F4

--- core/designsystem/src/main/java/com/linkedlist/linkllet/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/linkedlist/linkllet/core/designsystem/theme/Color.kt
@@ -12,3 +12,6 @@ val Pink40 = Color(0xFF7D5260)
 
 val ColorE9E9E9 = Color(0xFFE9E9E9) // 색 이름 정해질 때까지만 Color + RGB로 하겠습니다!
 val ColorC0C0C0 = Color(0xFFC0C0C0)
+val ColorE0E0E0 = Color(0xFFE0E0E0)
+val ColorBCBCBC = Color(0xFFBCBCBC)
+val ColorF4F4F4 = Color(0xFFF4F4F4)
