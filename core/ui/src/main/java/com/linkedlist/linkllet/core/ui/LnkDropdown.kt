8b63bb4857863ebcf0c3ae400073a7dc2ec7cae3
eshc123 <eshc@naver.com>
2023-07-15 23:55:51 +0900

    [feat] LnkDropdown - 크기, 텍스트 크기, 이미지 수정

--- core/ui/src/main/java/com/linkedlist/linkllet/core/ui/LnkDropdown.kt
+++ core/ui/src/main/java/com/linkedlist/linkllet/core/ui/LnkDropdown.kt
@@ -6,30 +6,36 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntRect
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupPositionProvider
+import com.linkedlist.linkllet.core.designsystem.icon.LnkIcon
+import com.linkedlist.linkllet.core.designsystem.icon.lnkicon.BtnRadio
+import com.linkedlist.linkllet.core.designsystem.icon.lnkicon.IcDropdownArrow
 
 @Composable
 fun LnkDropdownTextMenu(
@@ -61,6 +67,7 @@ fun LnkDropdownTextMenu(
             LnkDropdownTextItem(
                 itemText = selectedText,
                 isHeader = true,
+                isFocused = isFocused,
                 onSelect = {
                     onMenuClick()
                 }
@@ -70,7 +77,7 @@ fun LnkDropdownTextMenu(
                 Divider()
                 LazyColumn(
                     modifier = Modifier.height(
-                        (54 * (if (items.size > 4) 4 else items.size)).dp
+                        (49 * (if (items.size > 4) 4 else items.size)).dp
                     )
                 ) {
                     items(items) {
@@ -93,12 +100,13 @@ fun LnkDropdownTextItem(
     modifier: Modifier = Modifier,
     itemText: String,
     isHeader: Boolean = false,
+    isFocused: Boolean = false,
     selected: Boolean = false,
     onSelect: (String) -> Unit = {}
 ) {
     Row(
         modifier = modifier
-            .wrapContentHeight()
+            .height(49.dp)
             .clickable {
                 onSelect(itemText)
             }
@@ -108,22 +116,48 @@ fun LnkDropdownTextItem(
             modifier = Modifier
                 .wrapContentHeight()
                 .weight(1f),
-            text = itemText
+            text = itemText,
+            fontSize = 12.sp
         )
         Spacer(modifier = Modifier.width(32.dp))
-        Icon(
-            imageVector = if (isHeader) Icons.Filled.ArrowDropDown
-            else if (selected) Icons.Filled.CheckCircle
-            else Icons.Filled.Add,
-            contentDescription = null
-        )
+        if(!isHeader && !selected){
+            Spacer(modifier = Modifier.size(16.dp))
+        }else {
+            Icon(
+                modifier = Modifier
+                    .rotate(
+                        if (isHeader && !isFocused) 180f else 0f
+                    )
+                    .size(16.dp),
+                imageVector = if (isHeader) LnkIcon.IcDropdownArrow
+                else LnkIcon.BtnRadio,
+                contentDescription = null
+            )
+
+        }
     }
 }
 
 @Preview
 @Composable
 fun LnkDropdownTextMenuPreview() {
+    var selectedText by remember{ mutableStateOf("기본") }
+    var isFocused by remember { mutableStateOf(false) }
     MaterialTheme() {
-        LnkDropdownTextMenu(selectedText = "기본")
+        LnkDropdownTextMenu(
+            selectedText = selectedText,
+            onItemClick = {
+                selectedText = it
+                isFocused = false
+            },
+            isFocused = isFocused,
+            onMenuClick = {
+                isFocused = !isFocused
+            },
+            items = listOf("기본","UIUX","개발","서버","디자인","안드","iOS"),
+            onDismissRequest = {
+                isFocused = false
+            }
+        )
     }
 }
