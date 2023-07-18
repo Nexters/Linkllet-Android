961bd1d43ff156ac48c12b732ae9bf9da3224749
algosketch <algosketch@gmail.com>
2023-07-18 20:49:51 +0900

    [add] Retrofit Service를 쉽게 생성하기 위한 편의 객체

---
+++ core/data/src/main/java/com/linkedlist/linkellet/core/data/RetrofitFactory.kt
@@ -0,0 +1,31 @@
+package com.linkedlist.linkellet.core.data
+
+import com.linkedlist.linkllet.core.data.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitFactory {
+    private const val apiVersion = "v1"
+    const val baseUrl = "${BuildConfig.BACKEND_URL}/$apiVersion/"
+
+    inline fun <reified Service> create(): Service {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(createLoggingInterceptor())
+                    .build()
+            )
+            .build()
+            .create(Service::class.java)
+    }
+
+    fun createLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+}
