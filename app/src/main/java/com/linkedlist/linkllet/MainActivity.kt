ce7ca7ee43cf685b658db91d3a1774e590ddd9c1
Jung SeokJoon <eshc.joon@gmail.com>
2023-07-19 08:31:37 +0900

    [feat] MainViewModel 추가

--- app/src/main/java/com/linkedlist/linkllet/MainActivity.kt
+++ app/src/main/java/com/linkedlist/linkllet/MainActivity.kt
@@ -3,14 +3,20 @@ package com.linkedlist.linkllet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.linkedlist.linkllet.core.designsystem.theme.LinklletTheme
 import com.linkedlist.linkllet.ui.LnkApp
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
