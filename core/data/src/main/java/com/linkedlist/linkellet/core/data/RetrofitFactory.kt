5b17b867ac02d8d79e6a006cc44c6a1a4be0587b
Jung SeokJoon <eshc.joon@gmail.com>
2023-07-19 20:47:10 +0900

    [chore] 주석 삭제

--- core/data/src/main/java/com/linkedlist/linkellet/core/data/RetrofitFactory.kt
+++ core/data/src/main/java/com/linkedlist/linkellet/core/data/RetrofitFactory.kt
@@ -7,8 +7,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitFactory {
-    //private const val apiVersion = "v1"
-    //const val baseUrl = "${BuildConfig.BACKEND_URL}/api/$apiVersion/"
 
     inline fun <reified Service> create(): Service {
         return Retrofit.Builder()
