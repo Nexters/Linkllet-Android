eb5140c4cd95474ef6ee72c350cc21d61d1da19d
algosketch <algosketch@gmail.com>
2023-07-21 01:31:19 +0900

    [fix] 회원가입시 id 값이 잘못 들어가는 버그 수정

--- core/data/src/main/java/com/linkedlist/linkellet/core/data/source/remote/AuthRemoteDataSourceImpl.kt
+++ core/data/src/main/java/com/linkedlist/linkellet/core/data/source/remote/AuthRemoteDataSourceImpl.kt
@@ -1,20 +1,26 @@
 package com.linkedlist.linkellet.core.data.source.remote
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.provider.Settings
 import android.util.Log
 import com.linkedlist.linkellet.core.data.model.Auth
 import com.linkedlist.linkellet.core.data.model.request.SignUpRequest
 import com.linkedlist.linkellet.core.data.source.remote.api.AuthService
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class AuthRemoteDataSourceImpl @Inject constructor(
+    @ApplicationContext private val context: Context,
     private val authService: AuthService
 ) : AuthRemoteDataSource {
+
+    @SuppressLint("HardwareIds")
     override suspend fun signUp(): Result<Auth> {
         return try {
             val response = authService.signUp(
                 SignUpRequest(
-                    deviceId = Settings.Secure.ANDROID_ID
+                    deviceId = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
                 )
             )
             if(response.isSuccessful){
