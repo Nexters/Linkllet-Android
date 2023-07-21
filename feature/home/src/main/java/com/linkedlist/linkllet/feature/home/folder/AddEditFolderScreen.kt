bf1412ff8dcd5eaf6820e57a63c24c0c8c895271
algosketch <algosketch@gmail.com>
2023-07-21 21:29:03 +0900

    [feat] folder 생성 페이지 기본 UI 배치

--- feature/home/src/main/java/com/linkedlist/linkllet/feature/home/folder/AddEditFolderScreen.kt
+++ feature/home/src/main/java/com/linkedlist/linkllet/feature/home/folder/AddEditFolderScreen.kt
@@ -1,9 +1,11 @@
 package com.linkedlist.linkllet.feature.home.folder
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -14,8 +16,10 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.linkedlist.linkllet.core.designsystem.icon.LnkIcon
 import com.linkedlist.linkllet.core.designsystem.icon.lnkicon.X
@@ -34,7 +38,7 @@ fun AddEditFolderScreen(
 
     LaunchedEffect(key1 = Unit) {
         viewModel.eventFlow.collect {
-            when(it) {
+            when (it) {
                 is Event.CloseScreen -> onBack()
                 else -> {}
             }
@@ -45,23 +49,34 @@ fun AddEditFolderScreen(
         topBar = {
             LnkAppBar(
                 title = { AppBarTitle() },
-                action = { Close(onBack = onBack) }
+                action = { Close(onBack = onBack) },
+                modifier = Modifier.shadow(elevation = 4.dp), // fixme : 임시 그림자
             )
         }
     ) { padding ->
-        Column {
+        Column(
+            modifier = Modifier.background(color = Color.White)
+        ) {
             Box(
                 Modifier
                     .padding(padding)
+                    .padding(top = 30.dp)
+                    .padding(horizontal = 16.dp)
                     .fillMaxWidth(),
             ) {
                 LnkTextFieldWithTitle(
+                    modifier = Modifier.fillMaxWidth(),
                     title = "폴더 제목", value = folderName,
-                    onValueChange = viewModel::updateFolderName
+                    onValueChange = viewModel::updateFolderName,
+                    isVisibleMaxLengthNotice = true,
+                    maxLength = 10,
                 )
             }
             Spacer(modifier = Modifier.weight(1.0f))
             LnkButton(
+                modifier = Modifier
+                    .padding(horizontal = 24.dp, vertical = 14.dp)
+                    .height(50.dp),
                 onClick = viewModel::addFolder,
                 buttonColor = Color.Black,
                 text = "저장하기",
