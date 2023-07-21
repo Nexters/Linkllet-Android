f946dc80e418ff1ba979f6c8360f04d670b4c0b6
Jung SeokJoon <eshc.joon@gmail.com>
2023-07-21 22:48:24 +0900

    [feat] LinkViewModel - fetchLinks, deleteFolder, deleteLink 추가

--- feature/link/src/main/java/com/linkedlist/linkllet/feature/link/links/LinksViewModel.kt
+++ feature/link/src/main/java/com/linkedlist/linkllet/feature/link/links/LinksViewModel.kt
@@ -1,37 +1,132 @@
 package com.linkedlist.linkllet.feature.link.links
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.linkedlist.linkellet.core.data.repository.LinkRepository
+import com.linkedlist.linkllet.feature.link.navigation.FOLDER_ID
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
 data class Link(
+    val id : Long,
     val title : String,
     val link : String,
     val date : String
 )
 
 data class LinksUiState(
-    val links: List<Link> = linksDummy,
-    val isLoading: Boolean = false
+    val links: List<Link> = emptyList(),
+    val isLoading: Boolean = false,
+    val isFolderDeleted : Boolean = false,
+    val isLinkDeleted : Boolean = false
 )
 
-val linksDummy = listOf(
-    Link("자소서 참고1","https://brunch.co.kr/@plusx/98brunch.co.kr/@plusx/98","2023.7.8"),
-    Link("자소서 참고2","https://brunch.co.kr/@plusx/98brunch.co.kr/@plusx/99","2023.7.9"),
-    Link("자소서 참고3","https://brunch.co.kr/@plusx/98brunch.co.kr/@plusx/100","2023.7.10"),
-    Link("자소서 참고4","https://brunch.co.kr/@plusx/98brunch.co.kr/@plusx/101","2023.7.11"),
-    Link("자소서 참고1","https://brunch.co.kr/@plusx/98brunch.co.kr/@plusx/98","2023.7.8"),
-    Link("자소서 참고2","https://brunch.co.kr/@plusx/98brunch.co.kr/@plusx/99","2023.7.9"),
-    Link("자소서 참고3","https://brunch.co.kr/@plusx/98brunch.co.kr/@plusx/100","2023.7.10"),
-    Link("자소서 참고4","https://brunch.co.kr/@plusx/98brunch.co.kr/@plusx/101","2023.7.11")
-)
+@HiltViewModel
+class LinksViewModel @Inject constructor(
+    private val savedStateHandle: SavedStateHandle,
+    private val linkRepository: LinkRepository
+) : ViewModel() {
 
-
-class LinksViewModel : ViewModel() {
+    private val folderId = savedStateHandle.get<Long?>(
+        key = FOLDER_ID
+    )
 
     private val _uiState = MutableStateFlow(LinksUiState())
     val uiState: StateFlow<LinksUiState> = _uiState.asStateFlow()
 
+    fun fetchLinks() {
+        viewModelScope.launch {
+            try {
+                folderId?.let {
+                    linkRepository.getLinks(
+                        id = it
+                    ).catch {
+                        //error
+                    }.collect {
+                        it.onSuccess {
+                            _uiState.emit(
+                                uiState.value.copy(
+                                    links = it.map {
+                                        Link(
+                                            id = it.id,
+                                            title = it.name,
+                                            link = it.url,
+                                            date = "2023.07.20"
+                                        )
+                                    }
+                                )
+                            )
+                        }.onFailure {
+                            //error
+                        }
+                    }
+                }
+
+            }catch (e : Exception){
+                //error
+            }
+        }
+    }
+
+    fun deleteFolder() {
+        viewModelScope.launch {
+            try {
+                folderId?.let {
+                    linkRepository.deleteFolder(
+                        id = it
+                    ).catch {
+                        //error
+                    }.collect {
+                        it.onSuccess {
+                            _uiState.emit(
+                                uiState.value.copy(
+                                    isFolderDeleted = true
+                                )
+                            )
+                        }.onFailure {
+                            //error
+                        }
+                    }
+                }
+
+            }catch (e : Exception){
+                //error
+            }
+        }
+    }
+
+    fun deleteLink(linkId : Long) {
+        viewModelScope.launch {
+            try {
+                folderId?.let {
+                    linkRepository.deleteLink(
+                        id = it,
+                        articleId = linkId
+                    ).catch {
+                        //error
+                    }.collect {
+                        it.onSuccess {
+                            _uiState.emit(
+                                uiState.value.copy(
+                                    isLinkDeleted = true
+                                )
+                            )
+                        }.onFailure {
+                            //error
+                        }
+                    }
+                }
+
+            }catch (e : Exception){
+                //error
+            }
+        }
+    }
 }
