139f695384211f602206805738b4a2a8b62defba
algosketch <algosketch@gmail.com>
2023-07-29 01:57:33 +0900

    [add] 오른쪽 화살표 아이콘 추가

---
+++ core/designsystem/src/main/java/com/linkedlist/linkllet/core/designsystem/icon/lnkicon/Arrowright.kt
@@ -0,0 +1,42 @@
+package com.linkedlist.linkllet.core.designsystem.icon.lnkicon
+
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.linkedlist.linkllet.core.designsystem.icon.LnkIcon
+
+public val LnkIcon.ArrowRight: ImageVector
+    get() {
+        if (_arrowRight != null) {
+            return _arrowRight!!
+        }
+        _arrowRight = Builder(name = "ArrowRight", defaultWidth = 16.0.dp, defaultHeight = 16.0.dp,
+                viewportWidth = 16.0f, viewportHeight = 16.0f).apply {
+            path(fill = SolidColor(Color(0x00000000)), stroke = SolidColor(Color(0xFF000000)),
+                    strokeLineWidth = 2.0f, strokeLineCap = Butt, strokeLineJoin = Miter,
+                    strokeLineMiter = 4.0f, pathFillType = NonZero) {
+                moveTo(6.0f, 14.0f)
+                lineTo(11.0f, 8.0f)
+                lineTo(6.0f, 2.0f)
+            }
+        }
+        .build()
+        return _arrowRight!!
+    }
+
+private var _arrowRight: ImageVector? = null
+
+@Composable
+@Preview
+fun ArrowRightPreview() {
+    Icon(imageVector = LnkIcon.ArrowRight, contentDescription = "")
+}
