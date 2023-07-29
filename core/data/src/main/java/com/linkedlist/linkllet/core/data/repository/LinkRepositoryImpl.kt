d409a2f422b1a9ad984669d953a9c5b155acc4d8
eshc123 <eshc@naver.com>
2023-07-30 08:57:55 +0900

    [refactor] repository - 수정

--- core/data/src/main/java/com/linkedlist/linkllet/core/data/repository/LinkRepositoryImpl.kt
+++ core/data/src/main/java/com/linkedlist/linkllet/core/data/repository/LinkRepositoryImpl.kt
@@ -12,64 +12,39 @@ class LinkRepositoryImpl @Inject constructor(
     private val linkRemoteDataSource: LinkRemoteDataSource
 ) : LinkRepository {
     override suspend fun addFolder(name: String): Flow<Result<Unit>> = flow {
-        linkRemoteDataSource.addFolder(name = name)
-            .onSuccess {
-                emit(Result.success(Unit))
-            }.onFailure {
-                emit(Result.failure(it))
-            }
+        emit(linkRemoteDataSource.addFolder(name = name))
     }
 
     override suspend fun getFolders(): Flow<Result<List<Folder>>> = flow {
-        linkRemoteDataSource.getFolders()
-        .onSuccess {
-            emit(Result.success(it))
-        }.onFailure {
-            emit(Result.failure(it))
-        }
+        emit(linkRemoteDataSource.getFolders())
     }
 
     override suspend fun getLinks(id: Long): Flow<Result<List<Link>>> = flow {
-        linkRemoteDataSource.getLinks(id)
-            .onSuccess {
-                emit(Result.success(it))
-            }.onFailure {
-                emit(Result.failure(it))
-            }
+        emit(linkRemoteDataSource.getLinks(id))
     }
 
     override suspend fun addLink(id: Long, name: String, url: String): Flow<Result<Unit>> = flow {
-        linkRemoteDataSource.addLink(
-            id = id,
-            addLinkRequest = AddLinkRequest(
-                name = name,
-                url = url
+        emit(
+            linkRemoteDataSource.addLink(
+                id = id,
+                addLinkRequest = AddLinkRequest(
+                    name = name,
+                    url = url
+                )
             )
-        ).onSuccess {
-            emit(Result.success(Unit))
-        }.onFailure {
-            emit(Result.failure(it))
-        }
+        )
     }
 
     override suspend fun deleteFolder(id: Long): Flow<Result<Unit>> = flow {
-        linkRemoteDataSource.deleteFolder(
-            id = id
-        ).onSuccess {
-            emit(Result.success(Unit))
-        }.onFailure {
-            emit(Result.failure(it))
-        }
+        emit(linkRemoteDataSource.deleteFolder(id = id))
     }
 
     override suspend fun deleteLink(id: Long, articleId: Long): Flow<Result<Unit>> = flow {
-        linkRemoteDataSource.deleteLink(
-            id = id,
-            articleId = articleId
-        ).onSuccess {
-            emit(Result.success(Unit))
-        }.onFailure {
-            emit(Result.failure(it))
-        }
+        emit(
+            linkRemoteDataSource.deleteLink(
+                id = id,
+                articleId = articleId
+            )
+        )
     }
 }
