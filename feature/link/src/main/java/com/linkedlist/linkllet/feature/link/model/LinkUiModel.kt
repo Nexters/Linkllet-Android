25fde64514a743511010868182f0794ecf595a51
eshc123 <eshc@naver.com>
2023-08-04 06:44:13 +0900

    [refactor] 매핑 함수 추가

--- feature/link/src/main/java/com/linkedlist/linkllet/feature/link/model/LinkUiModel.kt
+++ feature/link/src/main/java/com/linkedlist/linkllet/feature/link/model/LinkUiModel.kt
@@ -1,8 +1,38 @@
 package com.linkedlist.linkllet.feature.link.model
 
+import android.annotation.SuppressLint
+import com.linkedlist.linkllet.core.data.model.Link
+import java.text.SimpleDateFormat
+import java.util.TimeZone
+
 data class LinkUiModel(
     val id : Long,
     val title : String,
     val link : String,
     val date : String
 )
+
+fun Link.toUiModel() : LinkUiModel =
+    LinkUiModel(
+        id = id,
+        title = name,
+        link = url,
+        date = formatDateString(createAt)
+    )
+
+@SuppressLint("SimpleDateFormat")
+fun formatDateString(input: String): String { /// 수정 필요.
+    return try {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        dateFormat.timeZone = TimeZone.getTimeZone("UTC")
+        val date = dateFormat.parse(input)
+
+        // 원하는 출력 형식으로 포맷팅할 SimpleDateFormat을 생성합니다.
+        val outputFormat = SimpleDateFormat("yyyy.MM.dd")
+
+        // Date 객체를 원하는 형식으로 포맷팅합니다.
+        outputFormat.format(date)
+    }catch (e: Exception){
+        ""
+    }
+}
