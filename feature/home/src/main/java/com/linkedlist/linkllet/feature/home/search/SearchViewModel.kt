21d5774c054dba13a570e4e7ae50e13e04457f47
algosketch <algosketch@gmail.com>
2023-08-12 00:40:11 +0900

    [feat] 검색 화면 UI 구성

--- feature/home/src/main/java/com/linkedlist/linkllet/feature/home/search/SearchViewModel.kt
+++ feature/home/src/main/java/com/linkedlist/linkllet/feature/home/search/SearchViewModel.kt
@@ -1,9 +1,33 @@
 package com.linkedlist.linkllet.feature.home.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+data class SearchUiState(
+    val keyword: String,
+)
 
 @HiltViewModel
 class SearchViewModel @Inject constructor() : ViewModel() {
+    private val _uiState = MutableStateFlow(SearchUiState(keyword = ""))
+    val uiState: StateFlow<SearchUiState> = _uiState.asStateFlow()
+
+    fun updateKeyword(newKeyword: String) {
+        _uiState.update {
+            uiState.value.copy(keyword = newKeyword)
+        }
+    }
+
+    fun search() {
+        viewModelScope.launch {
+
+        }
+    }
 }
