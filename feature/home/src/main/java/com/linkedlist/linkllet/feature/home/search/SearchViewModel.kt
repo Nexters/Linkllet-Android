1d9928beb7a311d43102e32859b7e2c6d65be78d
algosketch <algosketch@gmail.com>
2023-08-15 00:44:30 +0900

    [feat] 검색 결과 저장

--- feature/home/src/main/java/com/linkedlist/linkllet/feature/home/search/SearchViewModel.kt
+++ feature/home/src/main/java/com/linkedlist/linkllet/feature/home/search/SearchViewModel.kt
@@ -4,12 +4,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.linkedlist.linkllet.core.data.model.Folder
 import com.linkedlist.linkllet.core.data.model.Link
+import com.linkedlist.linkllet.core.data.repository.LinkRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -20,20 +20,23 @@ data class SearchUiState(
     val links: List<Link> = emptyList(),
 )
 
+sealed class SearchState {
+    object Loading : SearchState()
+    object BeforeToSearch : SearchState()
+    object Empty : SearchState()
+    object Success : SearchState()
+    object Error : SearchState()
+}
+
 @HiltViewModel
-class SearchViewModel @Inject constructor() : ViewModel() {
+class SearchViewModel @Inject constructor(
+    private val linkRepository: LinkRepository,
+) : ViewModel() {
     private val _uiState = MutableStateFlow(SearchUiState(keyword = ""))
     val uiState: StateFlow<SearchUiState> = _uiState.asStateFlow()
 
-    private val _loading = MutableStateFlow(false)
-    val loading: StateFlow<Boolean> = _loading.asStateFlow()
-
-    private val _beforeSearch = MutableStateFlow(true)
-    val beforeSearch: StateFlow<Boolean> = _beforeSearch.asStateFlow()
-
-    val empty: Flow<Boolean> = beforeSearch.map {
-        !beforeSearch.value && uiState.value.folders.isEmpty() && uiState.value.links.isEmpty()
-    }
+    private val _state: MutableStateFlow<SearchState> = MutableStateFlow(SearchState.BeforeToSearch)
+    val state: StateFlow<SearchState> = _state.asStateFlow()
 
     fun updateKeyword(newKeyword: String) {
         _uiState.update {
@@ -42,8 +45,25 @@ class SearchViewModel @Inject constructor() : ViewModel() {
     }
 
     fun search() {
+        val query = uiState.value.keyword
+        if(query.length < 2) return
+
         viewModelScope.launch {
-            _beforeSearch.value = false
+            linkRepository.search(query)
+                .onStart {
+                    _state.value = SearchState.Loading
+                }
+                .collect { result ->
+                    result.onSuccess { links ->
+                        _uiState.update {
+                            it.copy(links = links)
+                        }
+
+                        _state.value =
+                            if(links.isNotEmpty()) SearchState.Success
+                            else SearchState.Empty
+                    }
+                }
         }
     }
 }
