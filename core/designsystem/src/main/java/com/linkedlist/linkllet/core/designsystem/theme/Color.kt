b5881b031e2970da3fdcecaab31a454f961a95ad
algosketch <algosketch@gmail.com>
2023-08-15 20:56:26 +0900

    [fix] 타이포그라피 수정

--- core/designsystem/src/main/java/com/linkedlist/linkllet/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/linkedlist/linkllet/core/designsystem/theme/Color.kt
@@ -2,14 +2,6 @@ package com.linkedlist.linkllet.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val ColorE9E9E9 = Color(0xFFE9E9E9) // 색 이름 정해질 때까지만 Color + RGB로 하겠습니다!
 val ColorC0C0C0 = Color(0xFFC0C0C0)
 val ColorE0E0E0 = Color(0xFFE0E0E0)
