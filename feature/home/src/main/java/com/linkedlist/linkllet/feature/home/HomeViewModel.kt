85ebc2b01085c60464b56a2d21e7f01563d71b83
algosketch <algosketch@gmail.com>
2023-10-22 20:51:27 +0900

    [fix] 잘못된 에러 로직 수정 및 중복 코드 제거

--- feature/home/src/main/java/com/linkedlist/linkllet/feature/home/HomeViewModel.kt
+++ feature/home/src/main/java/com/linkedlist/linkllet/feature/home/HomeViewModel.kt
@@ -15,6 +15,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
@@ -61,13 +62,9 @@ class HomeViewModel @Inject constructor(
             }
             .onEach { folders ->
                 _uiState.update { it.copy(folders = folders) }
-                _loading.value = false
             }
-            .catch {
-                _loading.value = false
-                _error.value = false
-            }
-            .catch { _loading.value = false }
+            .catch { _error.value = true }
+            .onCompletion { _loading.value = false }
             .launchIn(viewModelScope)
     }
 
