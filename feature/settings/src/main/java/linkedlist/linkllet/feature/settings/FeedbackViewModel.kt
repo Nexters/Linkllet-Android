9a0d4f1b7adb7cd2d042be4e58fc6f1a13fac30b
algosketch <algosketch@gmail.com>
2023-10-29 01:02:28 +0900

    [fix] 피드백 중복 전송 방지 로직

--- feature/settings/src/main/java/linkedlist/linkllet/feature/settings/FeedbackViewModel.kt
+++ feature/settings/src/main/java/linkedlist/linkllet/feature/settings/FeedbackViewModel.kt
@@ -4,12 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.linkedlist.linkllet.core.data.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -28,15 +30,20 @@ class FeedbackViewModel @Inject constructor(
     private val _eventsFlow = MutableSharedFlow<FeedbackEvent>()
     val eventsFlow: SharedFlow<FeedbackEvent> = _eventsFlow.asSharedFlow()
 
+    private var postFeedbackJob: Job? = null
+
     fun updateContent(newContent: String) {
         _content.value = newContent
     }
 
     fun sendFeedback() {
-        authRepository.addFeedback(content = content.value)
+        if(postFeedbackJob != null) return
+
+        postFeedbackJob = authRepository.addFeedback(content = content.value)
             .onEach {
                 _eventsFlow.emit(FeedbackEvent.CloseScreen)
             }
+            .onCompletion { postFeedbackJob = null }
             .launchIn(viewModelScope)
     }
 }
